3decab73d037e6bc244edfe36dfee7c2770e7fe3
Zqw <578159259@qq.com>
2017-07-06 09:55:34 +0800

    config协议

---
+++ js/PublicProtocol.js
@@ -0,0 +1,23 @@
+//
+
+//获取界面需要的config参数
+var getConfigParameter = function(callback) {
+	var url = window.location.href;
+	var cncodeUrl = encodeURIComponent(url);
+	jQuery.post('https://jsypay.jiaobaowang.net/wxth/jssdkapi.aspx', {
+		reuri: cncodeUrl
+	}, function(data) {
+		callback(JSON.parse(data));
+	});
+}
+
+var sendConfigPro = function(configmsg, apiList) {
+	wx.config({
+		debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		appId: configmsg.appId, // 必填，公众号的唯一标识
+		timestamp: configmsg.timestamp, // 必填，生成签名的时间戳
+		nonceStr: configmsg.nonceStr, // 必填，生成签名的随机串
+		signature: configmsg.signature, // 必填，签名
+		jsApiList: apiList // 必填，需要使用的JS接口列表
+	});
+}

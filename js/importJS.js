f3d63face79532cba31bf8139a4e391c22bc8f5c
Zqw <578159259@qq.com>
2017-07-06 17:18:46 +0800

    引入js修改

---
+++ js/importJS.js
@@ -0,0 +1,4 @@
+//此文件用于导入js文件用，因为微信浏览器引入js时，容易有缓存
+
+document.write("<s" + "cript type='text/javascript' src='../../js/PublicProtocol.js?" + Math.random() + "'></s" + "cript>");
+document.write("<s" + "cript type='text/javascript' src='../../js/utils/utils.js?" + Math.random() + "'></s" + "cript>");

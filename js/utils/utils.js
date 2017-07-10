ebe0c06c2d2fd58679d4d950306148164a9aaf6d
BugHunter1234 <444811716@qq.com>
2017-07-10 10:34:31 +0800

    将网站配置的输入框和开关做成组件

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,12 +1,12 @@
 //出错的监听
 window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
-	//	console.log("---ERROR---start---");
-	//	console.log("错误信息-0:" + JSON.stringify(errorMessage.detail));
-	//	console.log("错误信息-1:" + errorMessage);
-	//	console.log("出错文件:" + scriptURI);
-	//	console.log("出错行号:" + lineNumber);
-	//	console.log("出错列号:" + columnNumber);
-	//	console.log("错误详情:" + errorObj);
+	console.log("---ERROR---start---");
+	console.log("错误信息-0:" + JSON.stringify(errorMessage.detail));
+	console.log("错误信息-1:" + errorMessage);
+	console.log("出错文件:" + scriptURI);
+	console.log("出错行号:" + lineNumber);
+	console.log("出错列号:" + columnNumber);
+	console.log("错误详情:" + errorObj);
 	alert("错误信息-0:" + JSON.stringify(errorMessage.detail) + "\n错误信息-1:" + errorMessage + "\n出错文件:" + scriptURI + "\n出错行号:" + lineNumber + "\n出错列号:" + columnNumber + "\n错误详情:" + errorObj);
 }
 

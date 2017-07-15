84452a1d68b8e31efc179701b03041d14f4b3c6c
Zqw <578159259@qq.com>
2017-07-15 12:33:40 +0800

    更新新闻主页

--- js/PublicProtocol.js
+++ js/PublicProtocol.js
@@ -26,7 +26,38 @@ var sendConfigPro = function(configmsg, apiList) {
 //发送对应的网站协议，根据页面传送的data
 var unitWebsitePro = function(data0, callback) {
 	jQuery.post('https://jsypay.jiaobaowang.net/wxth/appschweb/schwebapi.aspx', JSON.stringify(data0), function(data1) {
-		alert('返回值:'+data1);
-		callback(JSON.parse(data1));
+		//		alert('协议返回:'+JSON.stringify(data1));
+		console.log('协议返回:' + JSON.stringify(data1));
+		callback(data1);
 	});
+//	$.ajax({
+//		//提交数据的类型 POST GET
+//		type: "POST",
+//		//提交的网址
+//		url: "https://jsypay.jiaobaowang.net/wxth/appschweb/schwebapi.aspx",
+//		//提交的数据
+//		data: JSON.stringify(data0),
+//		//返回数据的格式
+//		datatype: "application/json", //"xml", "html", "script", "json", "jsonp", "text".
+//		//在请求之前调用的函数
+//		beforeSend: function() {
+//			
+//		},
+//		//成功返回之后调用的函数             
+//		success: function(data) {
+//			alert(JSON.stringify(data));
+////			callback(data);
+//		},
+//		//调用执行后调用的函数
+//		complete: function(XMLHttpRequest, textStatus) {
+////			alert(XMLHttpRequest.responseText);
+////			alert(textStatus);
+//			//HideLoading();
+//		},
+//		//调用出错执行的函数
+//		error: function(e) {
+//			alert('e:'+JSON.stringify(e));
+//			//请求出错处理
+//		}
+//	});
 }

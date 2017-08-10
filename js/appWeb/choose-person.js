1287711f521ad89779eb85362cb42629b0ef9839
rockan007 <rockan007@gmail.com>
2017-08-10 11:58:36 +0800

    获取部门信息

--- js/appWeb/choose-person.js
+++ js/appWeb/choose-person.js
@@ -1,29 +1,50 @@
 Vue.component("person-list", {
-	props: ['id'],
+	props: {
+		depart_id: {
+			type: Number,
+			default: -3
+		}
+	},
 	template: '<div v-if="loading">loading</div>' +
-		'<ul v-else>' +
+		'<ul v-else v-show="listData.length>0">' +
 		'<li v-for="(item,index) of listData"><p>{{item.name}}</p></li>' +
 		'</ul>',
+	watch: {
+		depart_id: function(newVal, oldVal) {
+			console.log('获取的新值：' + newVal)
+		}
+	},
+	ready: function() {
+		console.log("当前id值：" + this.depart_id);
+	},
 	data: function() {
 		return {
 			listData: [],
-			loading: false
+			loading: false,
+			id: this.depart_id
 		}
 	},
-	watch:{
-		id:function(val,old){
-			this.getListData();
-		}
+	created: function() {
+		console.log("获取的的id" + this.$route.params.id)
+		this.getListData();
 	},
-	computed: {
-		getListData: function() {
-			request.getDepartList(function(list) {
-				this.listData = list;
-				console.log("获取的部门信息：" + JSON.stringify(list));
-			}, this.id)
-		}
-	},
+
 	methods: {
+		getListData: function() {
+			var thisC = this;
+			var id;
+			if(thisC.id === -1) {
+				id = undefined;
+			} else {
+				id = thisC.id;
+			}
+			thisC.loading = true;
+			request.getDepartList(function(list) {
+				thisC.listData = list;
+				console.log("获取的部门信息：" + JSON.stringify(list));
+				thisC.loading = false;
+			}, id);
+		},
 		//通过部门id 更新界面
 		changeId: function(item) {
 			router.push({
@@ -34,7 +55,7 @@ Vue.component("person-list", {
 			});
 		},
 		//返回上个页面
-		goForword:function(){
+		goForword: function() {
 			router.go(-1);
 		}
 	}

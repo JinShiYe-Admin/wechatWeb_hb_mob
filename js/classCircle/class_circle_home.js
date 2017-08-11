74757dd30a7465218416d3bd180540346eadf48e
BugHunter1234 <444811716@qq.com>
2017-08-11 17:36:13 +0800

    调整班级圈样式

--- js/classCircle/class_circle_home.js
+++ js/classCircle/class_circle_home.js
@@ -1,9 +1,20 @@
 window.onload = function() {
 	console.log("window.onload");
-	$(document.body).pullToRefresh();
-	//	$(document.body).on("pull-to-refresh", function() {
-	//		setTimeout(function() {
-	//			$(document.body).pullToRefreshDone();
-	//		}, 2000);
-	//	});
+	$(".weui-tab__bd-item").pullToRefresh();
+	$(".weui-tab__bd-item").infinite();
+	$(".weui-tab__bd-item").on("pull-to-refresh", function() {
+		var self = this
+		setTimeout(function() {
+			$(self).pullToRefreshDone();
+		}, 2000)
+	});
+	var loading = false; //状态标记
+	$(".weui-tab__bd-item").infinite().on("infinite", function() {
+		if(loading) return;
+		loading = true;
+		setTimeout(function() {
+			$("#list").append("<p> 我是新加载的内容 </p>");
+			loading = false;
+		}, 1500); //模拟延迟
+	});
 }

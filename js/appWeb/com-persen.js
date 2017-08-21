a6daf1422ff9e9c3ab9caf0e005274f3f66e9d4c
rockan007 <rockan007@gmail.com>
2017-08-21 12:03:08 +0800

    人员选择逻辑

--- js/appWeb/com-persen.js
+++ js/appWeb/com-persen.js
@@ -1,21 +1,18 @@
 Vue.component('com-persen', {
-	template: '<div><router-view v-bind:class="[{\'margin-bottom50\':chosePersen.length>0}]"></router-view>'+
-	'<div v-if="chosePersen.length>0" v-bind:class="[\'weui-flex\']" v-bind:style="{position: \'fixed\',bottom: 0,height:\'50px\',width: \'100%\'}">' +
-	'<div v-bind:class="[\'container-wrap\',\'weui-flex__item\']">'+
-	'<div v-bind:class="[container]">' +
-	'<div v-for="person of chosePersen" v-bind:style="{display: \'inline-block\'}">' +
-	'<p v-bind:style="{display: \'inline-block\'}">{{person.name}}</p>' +
-	'</div>' +
-	'</div>' +
-	'</div>' +
-	'<a v-bind:class="[\'weui-btn\',\'weui-btn_mini\',\'weui-btn_primary\']" v-bind:style="{width:\'25%\',\'margin-bottom\': \'10px\'}">确定({{chosePersen.length>99?' +
-	'\"99+\":chosePersen.length}})</a>' +
-	'</div></div>',
+	props: ['chosePersen'],
+	template: '<div><router-view v-bind:class="[{\'margin-bottom50\':chosePersen.length>0}]"></router-view>' +
+		'<div v-if="chosePersen.length>0" v-bind:class="[\'weui-flex\']" v-bind:style="{position: \'fixed\',bottom: 0,height:\'50px\',width: \'100%\'}">' +
+		'<div v-bind:class="[\'container-wrap\',\'weui-flex__item\']">' +
+		'<div v-bind:class="[container]">' +
+		'<div v-for="person of chosePersen" v-bind:style="{display: \'inline-block\'}">' +
+		'<p v-bind:style="{display: \'inline-block\'}">{{person.name}}</p>' +
+		'</div>' +
+		'</div>' +
+		'</div>' +
+		'<a v-bind:class="[\'weui-btn\',\'weui-btn_mini\',\'weui-btn_primary\']" v-bind:style="{width:\'25%\',\'margin-bottom\': \'10px\'}">确定({{chosePersen.length>99?' +
+		'\"99+\":chosePersen.length}})</a>' +
+		'</div></div>',
 	data: function() {
-		
-		return {
-			chosePersen:[]
-		}
 	},
 	methods: {
 
--- js/appWeb/com-publish.js
+++ js/appWeb/com-publish.js
@@ -1,4 +1,5 @@
 Vue.component("com-publish", {
+	props: ['chosePersen'],
 	template: '<div><textarea v-bind:rows=10 v-bind:style="{width:\'100%\'}"></textarea>' +
 		'<div v-bind:class="[\'weui-cells\']">' +
 		'<div v-bind:class="[\'weui-cell\',\'weui-cell_access\']" v-on:click="routeToPersen">' +
@@ -6,17 +7,36 @@ Vue.component("com-publish", {
 		'人员选择' +
 		'</div>' +
 		'<div v-bind:class="[\'weui-cell__ft\']" >' +
-		//		'{{chosePersen.length>99?"99+":"chosePersen.length"}}' +
+		'{{chosePersen.length>99?"99+":chosePersen.length}}' +
 		'</div>' +
 		'</div>' +
 		'</div>' +
-		'<a v-bind:class="[\'weui-btn\', \'weui-btn_primary\']">发布</a></div>',
+		'<a v-bind:class="[\'weui-btn\', \'weui-btn_primary\']" v-on:click="publishMethod">发布</a></div>',
 	data: function() {
 		return {
-
+			title: '',
+			content: ''
 		}
 	},
 	methods: {
+		publishMethod: function() {
+			if(this.title.length == 0) {
+				mui.toast("请填写标题！");
+				return;
+			}
+			if(this.content.length == 0) {
+				mui.toast("请填写内容!");
+				return;
+			}
+			if(this.chosePersen.length == 0) {
+				mui.toast("请选择人员！");
+				return;
+			}
+			this.publish();
+		},
+		publish: function() {
+			console.log("发布事件！");
+		},
 		routeToPersen: function() {
 			console.log("导向新路由");
 			router.push('/persen/choose-person/-1')

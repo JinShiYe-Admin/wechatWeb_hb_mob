5ce803ee87ed8f50b5fbfddd21ab592c86422162
BugHunter1234 <444811716@qq.com>
2017-09-15 10:26:28 +0800

    隐藏页面发生错误时的弹出框

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -7,7 +7,7 @@ window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, err
 	console.log("出错行号:" + lineNumber);
 	console.log("出错列号:" + columnNumber);
 	console.log("错误详情:" + errorObj);
-	alert("错误信息-0:" + JSON.stringify(errorMessage.detail) + "\n错误信息-1:" + errorMessage + "\n出错文件:" + scriptURI + "\n出错行号:" + lineNumber + "\n出错列号:" + columnNumber + "\n错误详情:" + errorObj);
+	console.log("---ERROR---end---");
 }
 
 //公共方法

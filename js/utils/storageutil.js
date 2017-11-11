b78b6b23e3b7a776926f18b04a3610d437c9d194
BugHunter1234 <444811716@qq.com>
2017-11-11 09:41:48 +0800

    修改视频上传到七牛的逻辑，增加转格式，增加生成缩略图

--- js/utils/storageutil.js
+++ js/utils/storageutil.js
@@ -15,11 +15,14 @@ var storageutil = (function(mod) {
 	mod.QNQYWXKID = 9; //企业微信id
 	mod.QNQYWXKEY = "jsy927@"; //企业微信密钥
 	mod.QNQYWXFN = "wechat/"; //企业微信第一前缀名
-	mod.QNTHUMB = "thumb/"; //缩略图的第三前缀
-	mod.QNCROP = "crop/"; //裁剪图的第三前缀
 	mod.QNSSPACEWEBCON = "webcon/"; //网站配置空间(第二前缀名)
 	mod.QNSSPACECLASSCIRCLE = "classcircle/"; //班级圈空间(第二前缀名)
 	mod.QNBANNERPICTURE = "bannerpicture/"; //横幅图片(第二前缀名)
+	mod.QNVIDEO = "video/"; //视频(第二前缀名)
+	mod.QNTHUMB = "thumb/"; //缩略图的第三前缀
+	mod.QNCROP = "crop/"; //裁剪图的第三前缀
+	mod.QNCONVERT= "convert/"; //视频转换的第三前缀
+
 	//---七牛key值---end---
 
 	mod.WEBSITECONFIG = 'websiteconfig' //网站配置

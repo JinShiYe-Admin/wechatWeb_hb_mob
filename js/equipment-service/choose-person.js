002f910254e9de4420e82af10b9c94dd079fb1c6
rockan007 <rockan007@gmail.com>
2017-11-29 16:49:48 +0800

    维修服务项目 接口对接

--- js/equipment-service/choose-person.js
+++ js/equipment-service/choose-person.js
@@ -3,12 +3,16 @@ Vue.component("choose-person", {
 	props: {
 		choseDepart: {
 			type: Object
+		},
+		choosePerson: {
+			type: Object
 		}
 	},
 	data: function() {
 		return {
 			isAllCheck: false,
-			personList: []
+			personList: [],
+			selectPerson: {}
 		}
 	},
 	watch: {
@@ -16,13 +20,39 @@ Vue.component("choose-person", {
 			this.personList = [];
 			this.isAllCheck = false;
 			this.requireDepartPerson();
+		},
+		choosePerson: function(newVal, oldVal) {
+			console.log("choosePerson新值:" + JSON.stringify(newVal));
+			console.log("choosePerson旧值:" + JSON.stringify(oldVal));
+			this.selectPerson = newVal;
+			this.setChooseStatus();
 		}
 	},
 	methods: {
+		setChooseStatus: function() {
+			var com = this;
+			this.personList.forEach(function(person, index, personList) {
+				Vue.set(personList[index], "isCheck", !!com.selectPerson[person.userid])
+			})
+		},
+		updateSelectPerson: function() {
+			this.$emit("select-person", this.selectPerson);
+		},
+		toggleChoosePerson: function(person, index) {
+			console.log("toggleChoosePerson:" + JSON.stringify(person))
+			Vue.set(this.personList[index], "isCheck", !person.isCheck);
+			if(person.isCheck) {
+				this.selectPerson[person.userid] = person.name;
+			} else {
+				delete this.selectPerson[person.userid];
+			}
+			this.updateSelectPerson();
+		},
 		requireDepartPerson: function() {
 			var com = this;
 			request.getDepartPersons(this.choseDepart, 1, 0, function(data) {
 				com.personList = data;
+				com.setChooseStatus();
 			})
 		},
 		getTotalCheck: function(e) {

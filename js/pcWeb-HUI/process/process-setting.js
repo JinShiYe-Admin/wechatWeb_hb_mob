ad126e1a2459ce2052c20e9c36a633f2f3bb2476
rockan007 <rockan007@gmail.com>
2017-12-04 10:58:01 +0800

    添加流程接口和修改逻辑

--- js/pcWeb-HUI/process/process-setting.js
+++ js/pcWeb-HUI/process/process-setting.js
@@ -25,6 +25,11 @@ Vue.component("process-setting", {
 				ProTypeNote: "流程4备注" //流程备注
 			},
 			changeType: 0, //0 新建流程 1修改流程
+			corpId: 0,
+			pageIndex: 1,
+			totalPage: 1,
+			name: "",
+			note: ""
 		}
 	},
 	methods: {
@@ -32,8 +37,8 @@ Vue.component("process-setting", {
 		addProcess: function() {
 			//todo 添加流程
 			this.changeType = 0;
-			this.activeProcess.ProcTypeName = "";
-			this.activeProcess.ProTypeNote = "";
+			this.name = "";
+			this.note = "";
 			this.toggleLayer(true, "添加流程");
 		},
 		toggleLayer: function(isOpen, title) {
@@ -50,7 +55,10 @@ Vue.component("process-setting", {
 			}
 
 		},
-		//更改流程链
+		/**
+		 * 更改流程链
+		 * @param {Object} process 要更改流程链的流程
+		 */
 		changeProcessList: function(process) {
 			this.activeProcess = process;
 			console.log("更改流程链：" + JSON.stringify(process))
@@ -59,15 +67,88 @@ Vue.component("process-setting", {
 				name: 'chooseDepart'
 			})
 		},
-		//更改流程信息
+		/**
+		 * 更改流程信息
+		 * @param {Object} process
+		 */
 		changeProcessInfo: function(process) {
 			this.changeType = 1;
+			this.name = process.ProcTypeName;
+			this.note = process.ProTypeNote;
 			this.activeProcess = process;
 			this.toggleLayer(true, "更改流程信息");
 		},
-		//获取全部流程信息
-		requireAllProcess: function() {
+		submitProcess: function() {
+			if(changeType === 0) {
+				this.addProcessType();
+			} else {
+				this.setProcessType();
+			}
+		},
+		/**
+		 * 添加流程類型信息
+		 */
+		addProcessType: function() {
+			var com = this;
+			processRequest.postProcessData("addProcessType", {
+				corpId: this.corpId,
+				procTypeName: this.name,
+				proTypeNote: this.note
+			}, function(response) {
+				if(response.RspCode == 0) {
+					com.addDataToList(response.RspData.Result);
+				}
+			})
+		},
+		/**
+		 * 增加数据类型后，添加数据
+		 * @param {Object} processId
+		 */
+		addDataToList: function(processId) {
+			var process = {
+				ProcessTypeId: processId,
+				ProcTypeName: this.name,
+				ProTypeNote: this.note,
+				ProcessListId: -1,
+				ApprManList: [],
+				ProcNote: ""
+			}
+			if(this.pageIndex === 1) {
+				this.processList.splice(0, 0, process);
+				this.ProcessList.splice(20, 1)
+			} else {
+				this.requireProcess();
+			}
+		},
+		setProcessType: function() {
+			var com = this;
+			processRequest.postProcessData("setProcessType", {
+				procTypeId: this.activeProcess.ProcessTypeId,
+				procTypeName: this.name,
+				procTypeNote: this.note
+			}, function(response) {
+				if(response.RspCode == 0) {
+					com.activeProcess.ProcTypeName = this.name;
+					com.activeProcess.procTypeNote = this.note;
+				}
+			})
+		},
 
+		/**
+		 * 分页获取流程
+		 */
+		requireProcess: function() {
+			var com = this;
+			processRequest.postProcessData("getProcessList", {
+				corpId: this.corpId,
+				pageIndex: this.pageIndex,
+				pageSize: 20
+			}, function(response) {
+				if(response.RspCode == 0) {
+					com.totalPage = response.TotalPage;
+					com.processList = response.RspData.Data
+				}
+			})
 		},
 		//array to obj
 		changeArrayToObj: function(arrays) {
